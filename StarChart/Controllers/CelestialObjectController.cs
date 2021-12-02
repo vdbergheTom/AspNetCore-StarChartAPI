d3a553b34cdbabb6ce38ba2bbf7feffaf4ca4a07
vdbergheTom <vdberghe.tom@gmail.com>
2021-12-02 21:05:25 +0100

    context needs to be readonly

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -12,7 +12,7 @@ namespace StarChart.Controllers
     [ApiController]
     public class CelestialObjectController : ControllerBase
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
